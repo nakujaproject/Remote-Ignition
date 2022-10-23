dc78fec4e72e77855e0e26c23ef1d89b2e71154b
1998-felix <felix.gateru@gmail.com>
2022-10-23 10:20:07 +0300

    Progressing
    Signed-off-by: 1998-felix <felix.gateru@gmail.com>

--- Remote Ignition-Receiver/src/main.cpp
+++ Remote Ignition-Receiver/src/main.cpp
@@ -1,9 +1,58 @@
-#include <Arduino.h>
+// SimpleRx - the slave or the receiver
+
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+#define CE_PIN   9
+#define CSN_PIN 10
+
+const byte thisSlaveAddress[5] = {'R','x','A','A','A'};
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+char dataReceived[10]; // this must match dataToSend in the TX
+bool newData = false;
+
+//===========
 
 void setup() {
-  // put your setup code here, to run once:
+
+    Serial.begin(9600);
+
+    Serial.println("SimpleRx Starting");
+    radio.begin();
+    radio.setDataRate( RF24_250KBPS );
+    radio.openReadingPipe(1, thisSlaveAddress);
+    radio.startListening();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+
+void showData() {
+    if (newData == true) {
+        Serial.print("Data received ");
+        Serial.println(dataReceived);
+        newData = false;
+    }
 }
+//=============
+
+
+
+void getData() {
+    if ( radio.available() ) {
+        radio.read( &dataReceived, sizeof(dataReceived) );
+        newData = true;
+    }
+}
+
+
+void loop() {
+    getData();
+    showData();
+}
+
+//==============
+
+
