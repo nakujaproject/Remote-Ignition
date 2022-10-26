12bae56776fbdeb0d41b167339953f5b4cf38a25
TheParadox20 <onyambusamson@gmail.com>
2022-10-26 13:11:11 +0300

    seitch to esp

--- server/demo.py
+++ server/demo.py
@@ -1,8 +1,1 @@
 from rwCOM import COM
-
-serialCOM = COM('COM6')
-
-file = open('logs.txt')
-while 1:
-    # print(file.readline())
-    print(COM.getLatest())
--- server/rwCOM.py
+++ server/rwCOM.py
@@ -1,9 +1,10 @@
+from pickle import OBJ
 import serial
 import time
 from log import secretary
 
 #Format -> index, hx711_value
-class COM:
+class COM(object):
     latest = '' #holds the latest reading from the COM port
     def __init__(self, port):
         self.serialPort = serial.Serial(port = port, baudrate=9600,timeout=.2)
@@ -11,14 +12,6 @@ class COM:
     def getLatest():
         return COM.latest
     
-    def write(self,command: bytes):
-        self.serialPort.write(command)
-        time.sleep(0.9)
-        while 1:
-            if(self.serialPort.in_waiting > 0):
-                    print(self.serialPort.readline().decode('Ascii'))
-                    break
-        
     def main(self):
         while 1:
             if(self.serialPort.in_waiting > 0):
@@ -26,9 +19,8 @@ class COM:
                 COM.latest = self.serialPort.readline()
                 secretary(COM.latest)
                 # Print the contents of the serial data
-                print(COM.latest.decode('Ascii'))
+                print(COM.latest.decode('Ascii'),end='')
 
 if __name__=="__main__":
     serialCOM = COM('COM6')
-    # serialCOM.write((bytes('xammzy', 'utf-8')))
     serialCOM.main()
